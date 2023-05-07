b82f62b6c885036b163cd03d3c89e31b16c1b57f
varun146 <varungarg63683@gmail.com>
2023-05-07 05:31:06 -0400

    all tasks done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,42 +19,82 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) { 
-    res.send(JSON.stringify(books));
+    return new Promise((resolve, reject) => {
+        if(books){
+            resolve(books);
+        } else {
+            reject("No books");
+        }
+    })
+    .then(booklist => {
+        res.send(booklist);
+    })
+    .catch(err => {
+        res.send(err);
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   
     const isbn = req.params.isbn;
-    if(isbn){
-        res.send(JSON.stringify(books[isbn]));
-    } else {
-        res.send("ISBN not provided");
-    }
+    return new Promise((resolve, reject) => {
+        if(books[isbn]){
+            resolve(books[isbn]);
+        } else {
+            reject("No such book with this ISBN");
+        }      
+    })
+    .then((book) => {
+        res.send(book);
+    })
+    .catch((err) => {
+        res.send("No such book with this ISBN");
+    })
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    for(let book in books){
-        if(books[book].author === author){
-            res.send(JSON.stringify(books[book]));
+    return new Promise((resolve, reject) => {
+        for(let book in books){
+            if(books[book].author === author){
+                resolve(books[book]);
+            }
         }
-    }
-    res.send("No such author exists in the database;");
-
+        reject("No such author exists in the database");
+    })
+    .then((book) => {
+        console.log(book);
+        res.json(book);
+    })
+    .catch((err) => {
+        res.send(err);
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    for(let book in books){
-        if(books[book].title === title){
-            res.send(JSON.stringify(books[book]));
+    return new Promise((resolve, reject) => {
+        for(let book in books){
+            if(books[book].title === title){
+                resolve(books[book]);
+            }
         }
-    }
-    res.send("No such title exists in the database;");
+        reject("No such book with this title exists in the database");
+    })
+    .then((book) => {
+        console.log(book);
+        res.json(book);
+    })
+    .catch((err) => {
+        res.send(err);
+    });
+    
 });
+
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
